35456d7c893948f8782d9a2a784ce8fca60ad70a
Mitchel Humpherys <mitch.special@gmail.com>
2025-05-06 11:52:56 -0700

    ci: Comment about AUR build files
    The repo name isn't super clear.

--- actions/build-test-arch/entry.sh
+++ actions/build-test-arch/entry.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Clone the AUR build files
 git clone https://github.com/mgalgs/gnome-shell-extension-system-monitor-next-git.git
 cd gnome-shell-extension-system-monitor-next-git
 makepkg --install --noconfirm
