mgalgs/gnome-shell-system-monitor-next-applet

commit 6ff5cde10437778ba2d92fd7b1d37ef010eb0191

Makefile: Simplify and clean up
Many of our Makefile targets are not super helpful and some shouldn't
really be used at all. Uninstalling, for example, should typically be done
via the distro package manager or Gnome Shell's built-in extension
management tools.

This patch removes some of those not-so-helpful targets and cleans up some
formatting and conventions (no double-colon rules and PHONY var
accumulations interspersed throughout the file).

Also change the DESTDIR variable to the more conventional name of PREFIX so
that distro packagers can use:

    make PREFIX="${pkgdir}/usr" install

in their packaging scripts.

A full packaging script might look like this:

    build() {
        install -d "${pkgdir}/usr/share/gnome-shell/extensions"
        install -d "${pkgdir}/usr/share/glib-2.0/schemas"
        make PREFIX="${pkgdir}/usr" install
    }

The cached gsettings schema database should then be recompiled with:

    glib-compile-schemas /usr/share/glib-2.0/schemas

(which is often taken care of by the distro's package manager hooks, like
/usr/share/libalpm/hooks/glib-compile-schemas.hook in Arch Linux, for
example.)

Makefile
@@ -1,8 +1,8 @@
 # -*- coding: utf-8; mode: makefile-gmake -*-
-# Basic Makefile
 
 UUID = system-monitor-next@paradoxxx.zero.gmail.com
 INSTALLNAME = $(UUID)
+PREFIX ?= $(HOME)/.local
 
 BASE_MODULES = \
   $(UUID)/extension.js \
@@ -18,24 +18,12 @@ BASE_MODULES = \
 GSCHEMA_XML = $(UUID)/schemas/org.gnome.shell.extensions.system-monitor-next-applet.gschema.xml
 GSCHEMA_COMPILED = $(UUID)/schemas/gschemas.compiled
 
-# ---------
-# variables
-# ---------
+VERSION = $(shell git rev-parse HEAD)
+ZIPFILE = $(UUID).zip
 
-ifeq ($(strip $(DESTDIR)),)
-  INSTALLBASE = $(HOME)/.local/share/gnome-shell/extensions
-  SCHEMAINSTALLBASE = $(HOME)/.local/share/glib-2.0/schemas
-else
-  INSTALLBASE = $(DESTDIR)usr/share/gnome-shell/extensions
-  SCHEMAINSTALLBASE = $(DESTDIR)usr/share/glib-2.0/schemas
-endif
-
-ifdef VERSION
-  VSTRING = _v$(VERSION)
-else
-  VERSION = $(shell git rev-parse HEAD)
-  VSTRING =
-endif
+INSTALLBASE = $(PREFIX)/share/gnome-shell/extensions
+SCHEMAINSTALLBASE = $(PREFIX)/share/glib-2.0/schemas
+INSTALLDIR = $(INSTALLBASE)/$(INSTALLNAME)
 
 # VERBOSE level
 
@@ -54,92 +42,66 @@ endif
 # usage: $(call msg,INFO,'lorem ipsum')
 msg = @printf '  [%-12s] %s\n' '$(1)' '$(2)'
 
-
 # -------
 # targets
 # -------
 
-# is there anymore use of the (old) 'all' target?
-# PHONY += all
-# all: extension
-
-PHONY += help
 help:
-	@echo  'Install or remove the extension, for the local user'
-	@echo  'or as admin for all users:'
+	@echo  'Install the extension to ~/.local/share/ (for the local user):'
 	@echo  ''
-	@echo  '  make [install|remove]                        # for the local user'
-	@echo  '  make DESTDIR=/ [install|remove] clean        # as admin for all users'
+	@echo  '  make install'
 	@echo  ''
-	@echo  'Use environment VERSION=n.m to set verison string in the metadata and in'
-	@echo  'the generated zip-file explicit.  If no VERSION is passed, the current'
-	@echo  'commit SHA1 is used as version number in the metadata while the generated'
-	@echo  'zip file has no string attached.'
+	@echo  'Install the extension to $${PREFIX}/share/ (for system-wide install/packaging):'
 	@echo  ''
-	@echo  'Other targets are:'
+	@echo  '  sudo make PREFIX=/usr install       # as admin for all users'
+	@echo  '  make PREFIX=$${pkgdir}/usr install   # to another directory (for packaging)'
 	@echo  ''
-	@echo  '  zip-file  - build and zip ./$(UUID).zip'
+	@echo  'Other targets:'
+	@echo  ''
+	@echo  '  zip-file  - build $(ZIPFILE)'
+	@echo  '              (which can be uploaded to extensions.gnome.org or installed'
+	@echo  '               with gnome-extensions install)'
 	@echo  '  clean     - remove most generated files'
-	@echo  '  gschemas  - rebuild schemas/gschemas.compiled'
-	@echo  '  translate - generate translation from po/ files'
 	@echo  ''
-	@echo  'control verbosity:'
-	@echo  ''
-	@echo  '  make V=0 [targets] -> quiet build (default)'
-	@echo  '  make V=1 [targets] -> verbose build'
+	@echo  'Note that most users should install the extension via extensions.gnome.org'
+	@echo  'or their distro package manager.'
 
-
-PHONY += install remove
-
-install: build gschemas-install
-	$(call msg,$@,Installing to $(INSTALLBASE)/$(INSTALLNAME))
-	$(Q) mkdir -p $(INSTALLBASE)/$(INSTALLNAME)
-	$(Q) cp $(VV) -r ./_build/* $(INSTALLBASE)/$(INSTALLNAME)/
+install: clean build gschemas.install
+	$(call msg,$@,Installing to $(INSTALLDIR))
+	$(Q) mkdir -p "$(INSTALLDIR)"
+	$(Q) cp $(VV) -r ./_build/* "$(INSTALLDIR)/"
 	$(call msg,$@,OK)
 	$(call msg,$@,Please reload GNOME Shell and enable the extension)
 
-remove:
-	$(call msg,$@,Removing $(INSTALLBASE)/$(INSTALLNAME))
-ifeq ($(strip $(BUILD_FOR_RPM)),)
-	$(Q) gnome-extensions uninstall --quiet $(UUID) || true
-	$(Q) rm -rf $(INSTALLBASE)/$(INSTALLNAME) 2>/dev/null || true
-endif
-	$(call msg,$@,OK)
+uninstall:
+	$(Q)gnome-extensions uninstall $(UUID)
 
-PHONY += zip-file zip-file.clean
-ZIPFILE=$(UUID)$(VSTRING).zip
+clean: zip-file.clean gschemas.clean build.clean
 
-zip-file: build.clean build
+zip-file: clean build
 	$(Q)cd _build ; zip $(V) -qr $(ZIPFILE) .
 	$(Q)mkdir -p dist
 	$(Q)mv _build/$(ZIPFILE) ./dist/$(ZIPFILE)
 	$(call msg,$@,Zip file saved to ./dist/$(ZIPFILE))
 	$(call msg,$@,OK)
 
-zip-install: remove zip-file
-	$(Q)gnome-extensions install ./dist/$(ZIPFILE)
-	$(call msg,$@,OK)
-	$(call msg,$@,Please reload GNOME Shell)
-
-clean:: zip-file.clean
 zip-file.clean:
 	$(Q)rm $(VV) -vf ./dist/$(ZIPFILE)
 	$(call msg,$@,OK)
 
-
-PHONY += gschemas gschemas.clean _drop-gschemas
-
-gschemas: _drop-gschemas $(GSCHEMA_COMPILED)
+gschemas: $(GSCHEMA_COMPILED)
 	$(call msg,$@,OK)
 
-gschemas-install: $(GSCHEMA_XML)
-	$(Q)mkdir -p $(SCHEMAINSTALLBASE)
-	$(Q)cp $(VV) $(GSCHEMA_XML) $(SCHEMAINSTALLBASE)
-	$(Q)glib-compile-schemas $(SCHEMAINSTALLBASE)
-	$(call msg,$@,gschema installed to $(SCHEMAINSTALLBASE))
+gschemas.install: $(GSCHEMA_XML)
+	$(Q)mkdir -p "$(SCHEMAINSTALLBASE)"
+	$(Q)cp $(VV) $(GSCHEMA_XML) "$(SCHEMAINSTALLBASE)"
+	$(call msg,$@,gschema installed to $(SCHEMAINSTALLBASE). You might need to run "glib-compile-schemas $(SCHEMAINSTALLBASE)")
 	$(call msg,$@,OK)
 
-clean:: gschemas.clean
+gschemas.uninstall:
+	$(Q)rm -f "$(SCHEMAINSTALLBASE)/$(GSCHEMA_XML)"
+	$(Q)glib-compile-schemas "$(SCHEMAINSTALLBASE)"
+
 gschemas.clean:
 	$(Q)git checkout -f -- $(GSCHEMA_COMPILED)
 	$(call msg,$@,OK)
@@ -148,12 +110,6 @@ $(GSCHEMA_COMPILED): $(GSCHEMA_XML)
 	$(Q)glib-compile-schemas ./$(UUID)/schemas/
 	$(call msg,gschemas,OK)
 
-_drop-gschemas:
-	$(Q)rm -f $(GSCHEMA_COMPILED)
-
-
-PHONY += build build.clean
-
 build: gschemas translate
 	$(Q)mkdir -p _build
 	$(Q)cp $(VV) $(BASE_MODULES) _build
@@ -161,28 +117,28 @@ build: gschemas translate
 	$(Q)cp $(VV) -r $(UUID)/ui/* _build/ui/
 	$(Q)mkdir -p _build/locale
 	$(Q)cp $(VV) -r $(UUID)/locale/* _build/locale/
-	$(Q)mkdir -p _build/schemas
-	$(Q)cp $(VV) $(GSCHEMA_XML) _build/schemas/
-	$(Q)cp $(VV) $(GSCHEMA_COMPILED) _build/schemas/
 	$(Q)sed -i 's/"version": -1/"version": "$(VERSION)"/'  _build/metadata.json;
 	$(call msg,$@,Extension built, saved to: _build/)
 	$(call msg,$@,OK)
 
-clean:: build.clean
 build.clean:
-	$(Q)rm -fR ./_build
+	$(Q)rm -rf ./_build
 	$(call msg,$@,OK)
 
-PHONY += translate
-translate: gschemas
+translate:
 	$(Q)cd po;\
            ./compile.sh ../system-monitor-next@paradoxxx.zero.gmail.com/locale \
 	   | tr '\n' ' ' \
 	   | sed -e 's/^/  [$@   ] /;'; echo
 	$(call msg,$@,OK)
 
-clean:: translation.clean
-translation.clean:
-	$(Q)git checkout -f -- system-monitor-next@paradoxxx.zero.gmail.com/locale
-
-.PHONY: $(PHONY)
+.PHONY: help \
+	install \
+	zip-file \
+	zip-file.clean \
+	gschemas \
+	gschemas.clean \
+	gschemas.install \
+	build \
+	build.clean \
+	translate

commit a8ef9f770730f4f4984842847825212f1ad4f12b

build: Include schemas in _build directory
The Makefile cleanup in 6ff5cde1 ("Makefile: Simplify and clean up")
accidentally removed the lines that copied schemas/ into _build/. This
meant any zip built with `make zip-file` was missing schemas entirely,
breaking installs from extensions.gnome.org (which rely on schemas
being inside the extension directory).

Restore the schema copy in the build target and remove the workaround
in vm-deploy.sh that was rsyncing schemas separately.

Fixes: #143
Fixes: #144

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

Makefile
@@ -125,6 +125,8 @@ $(GSCHEMA_COMPILED): $(GSCHEMA_XML)
 build: gschemas translate
 	$(Q)mkdir -p _build
 	$(Q)cp $(VV) $(BASE_MODULES) _build
+	$(Q)mkdir -p _build/schemas
+	$(Q)cp $(VV) -r $(UUID)/schemas/* _build/schemas/
 	$(Q)mkdir -p _build/ui
 	$(Q)cp $(VV) -r $(UUID)/ui/* _build/ui/
 	$(Q)mkdir -p _build/locale

testing/vm/lib/vm-deploy.sh
@@ -22,11 +22,6 @@ deploy_extension() {
     vm_rsync "$vm_name" \
         "$PROJECT_ROOT/_build/" \
         "~/.local/share/gnome-shell/extensions/$EXT_UUID/"
-
-    # rsync schemas separately (not included in _build by the Makefile)
-    vm_rsync "$vm_name" \
-        "$PROJECT_ROOT/$EXT_UUID/schemas/" \
-        "~/.local/share/gnome-shell/extensions/$EXT_UUID/schemas/"
     log_ok "Extension deployed"
 
     # Compile schemas on VM (in extension dir and system-wide)